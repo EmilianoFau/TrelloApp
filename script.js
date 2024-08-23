253a537d98596d81b4628554be839cc9a2b1aee3
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-08-23 02:54:26 -0300

    a

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tasks = document.querySelectorAll('.task');
+    const modal = document.getElementById('modal');
+    const closeButton = document.querySelector('.close-button');
+    const saveButton = document.getElementById('save-button');
+    const modalTitle = document.getElementById('modal-title');
+    
+    tasks.forEach(task => {
+        task.addEventListener('click', () => {
+            modal.classList.remove('hidden');
+            modalTitle.textContent = task.textContent;
+        });
+    });
+
+
+    closeButton.addEventListener('click', () => {
+        modal.classList.add('hidden');
+    });
+
+    saveButton.addEventListener('click', () => {
+        // Implementar la lógica de guardar cambios
+        modal.classList.add('hidden');
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.classList.add('hidden');
+        }
+    });
+});
